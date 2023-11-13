63f381ee1f3ecdaaf61c3e9e9f77bd98db334d72
Francisco Ramirez <framirezjrp07@gmail.com>
2023-11-12 21:50:57 -0600

    update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 import { environment } from '@env/environment';
 import '@fontsource/carlito';
 
@@ -6,6 +7,8 @@ import '@fontsource/carlito';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+
+  animations: [trigger('logoOut', [transition(':leave', [animate('200ms', style({ opacity: 0 }))])])],
 })
 export class AboutComponent implements OnInit {
   slideOpts = {
@@ -18,18 +21,139 @@ export class AboutComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  // ------- -------
+  // Intro
+  // ------- -------
+  timeout = 500;
+  timeout1s = 1000;
+  introStarted = false;
+  introState = 0;
+  bgColor = 'white';
 
-  isHidden: boolean = true;
-  hoverItemId: string = '';
+  startIntro = async () => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.introState = 1;
+        this.bgColor = '#000';
+        resolve(true);
+      }, this.timeout1s);
+    });
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.introState = 1.1;
+        this.state2();
+      }, this.timeout1s * 2);
+    });
+  };
+
+  state2 = async () => {
+    setTimeout(() => {
+      this.introState = 2;
+      this.state3();
+    }, this.timeout1s);
+  };
+
+  state3 = async () => {
+    setTimeout(() => {
+      this.introState = 3;
+      this.state4();
+    }, this.timeout);
+  };
+
+  state4 = async () => {
+    setTimeout(() => {
+      this.introState = 4;
+      this.state5();
+    }, this.timeout);
+  };
+
+  state5 = async () => {
+    setTimeout(() => {
+      this.introState = 5;
+      this.state6();
+    }, this.timeout);
+  };
+
+  state6 = async () => {
+    setTimeout(() => {
+      this.introState = 6;
+      this.state7();
+    }, this.timeout);
+  };
+
+  state7 = async () => {
+    setTimeout(() => {
+      this.introState = 7;
+      this.state8();
+    }, this.timeout);
+  };
+
+  state8 = async () => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.introState = 8;
+        resolve(true);
+      }, this.timeout);
+    });
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.introState = 8.1;
+        this.bgColor = '#FFF';
+        this.state9();
+        resolve(true);
+      }, this.timeout1s);
+    });
+  };
+
+  state9 = async () => {
+    for (let i = 1; i <= 7; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          this.introState = 101 + i;
+          resolve(true);
+        }, this.timeout / 2);
+      });
+    }
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, this.timeout1s);
+    });
+
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.introState = 100;
+        resolve(true);
+      }, this.timeout1s);
+    });
+  };
+  // ------- -------
+
+  isHidden = true;
+  hoverItemId = '';
+  hoverLineId = 0;
 
   onMouseEnter(itemId: string) {
     this.hoverItemId = itemId;
-    console.log('@@ onEnter: hoverItemId: ', this.hoverItemId);
   }
 
   onMouseLeave() {
     this.hoverItemId = '';
-    console.log('@@ onLeave: hoverItemId: ', this.hoverItemId);
+  }
+
+  onLineEnter(itemId: number) {
+    this.hoverLineId = itemId;
+  }
+
+  onLineLeave() {
+    this.hoverLineId = 0;
+  }
+
+  ngOnInit() {
+    this.startIntro();
+    // this.state9()
   }
 }
